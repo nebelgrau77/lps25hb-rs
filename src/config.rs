e5cf17f0e7c2d4c8d08db2ee09432e04518aae8b
nebelgrau <nebelgrau@gmx.com>
2021-08-11 09:02:39 +0200

    added config.rs, have to move functions to their modules

---
+++ src/config.rs
@@ -0,0 +1,11 @@
+//! Various functions related to configuration
+
+
+use super::*; 
+
+impl<T, E> LPS25HB<T> 
+where
+    T: Interface<Error = E>,
+{   
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -266,46 +266,6 @@ where
     
     
 
-/// FIFO enable/disable
-pub fn fifo_config(&mut self, flag: Control) -> Result<(), T::Error> {
-    match flag {
-        Control::On => {
-            self.set_register_bit_flag(Registers::CTRL_REG2, FIFO_EN)
-        }
-        Control::Off => {
-            self.clear_register_bit_flag(Registers::CTRL_REG2, FIFO_EN)
-        }
-    }
-}
-
-
-/// Select FIFO operation mode (see Table 22 for details)        
-pub fn fifo_mode_config(&mut self, mode: FIFO_MODE) -> Result<(), T::Error> {
-    let mut reg_data = [0u8];
-    self.interface.read(Registers::FIFO_CTRL.addr(), &mut reg_data)?;
-    let mut payload = reg_data[0];
-    payload &= !F_MODE_MASK;
-    payload |= mode.value();
-    self.interface.write(
-        Registers::FIFO_CTRL.addr(),
-        payload,
-    )?;
-    Ok(())
-}
-
-/// Select sample size for FIFO Mean mode running average (see Table 23 for details)        
-pub fn fifo_mean_config(&mut self, sample: FIFO_MEAN) -> Result<(), T::Error> {
-    let mut reg_data = [0u8];
-    self.interface.read(Registers::FIFO_CTRL.addr(), &mut reg_data)?;
-    let mut payload = reg_data[0];
-    payload &= !WTM_POINT_MASK;
-    payload |= sample.value();
-    self.interface.write(
-        Registers::FIFO_CTRL.addr(),
-        payload,
-    )?;
-    Ok(())
-}
 
 
     /// Interrupt request latching to INT_SOURCE
@@ -344,41 +304,7 @@ pub fn fifo_mean_config(&mut self, sample: FIFO_MEAN) -> Result<(), T::Error> {
         }
     }
 
-    /// FIFO empty flag on INT_DRDY pin
-    pub fn fifo_empty_drdy_config(&mut self, flag: Control) -> Result<(), T::Error> {
-        match flag {
-            Control::On => {
-                self.set_register_bit_flag(Registers::CTRL_REG4, F_EMPTY)
-            }
-            Control::Off => {
-                self.clear_register_bit_flag(Registers::CTRL_REG4, F_EMPTY)
-            }
-        }
-    }
-
-    /// FIFO filled up to threshold (watermark) level on INT_DRDY pin 
-    pub fn fifo_filled_drdy_config(&mut self, flag: Control) -> Result<(), T::Error> {
-        match flag {
-            Control::On => {
-                self.set_register_bit_flag(Registers::CTRL_REG4, F_FTH)
-            }
-            Control::Off => {
-                self.clear_register_bit_flag(Registers::CTRL_REG4, F_FTH)
-            }
-        }
-    }
-
-    /// FIFO overrun interrupt on INT_DRDY pin 
-    pub fn fifo_overrun_drdy_config(&mut self, flag: Control) -> Result<(), T::Error> {
-        match flag {
-            Control::On => {
-                self.set_register_bit_flag(Registers::CTRL_REG4, F_OVR)
-            }
-            Control::Off => {
-                self.clear_register_bit_flag(Registers::CTRL_REG4, F_OVR)
-            }
-        }
-    }
+   
 
     /// Data-ready signal on INT_DRDY pin 
     pub fn data_signal_drdy_config(&mut self, flag: Control) -> Result<(), T::Error> {
@@ -433,20 +359,7 @@ pub fn fifo_mean_config(&mut self, sample: FIFO_MEAN) -> Result<(), T::Error> {
         self.is_register_bit_flag_high(Registers::STATUS_REG, T_DA)
     }
 
-    /// Is FIFO filling equal or higher than the threshold?
-    pub fn fifo_threshold_status(&mut self) -> Result<bool, T::Error> {
-        self.is_register_bit_flag_high(Registers::FIFO_STATUS, FTH_FIFO)
-    }
-
-    /// Is FIFO full and at least one sample has been overwritten?
-    pub fn fifo_overrun_status(&mut self) -> Result<bool, T::Error> {
-        self.is_register_bit_flag_high(Registers::FIFO_STATUS, OVR)
-    }
-
-    /// Is FIFO empty?
-    pub fn fifo_empty_status(&mut self) -> Result<bool, T::Error> {
-        self.is_register_bit_flag_high(Registers::FIFO_STATUS, EMPTY_FIFO)
-    }
+    
 
 
 
@@ -522,18 +435,4 @@ pub fn fifo_mean_config(&mut self, sample: FIFO_MEAN) -> Result<(), T::Error> {
 
 
      
-
-        /*
-        /// Write to a register.
-        fn write_register(&mut self, address: Registers, data: u8) -> Result<(), T::Error> {
-            let payload: [u8; 2] = [address, data]; 
-            self.interface.write(
-                address.addr(),
-                payload,
-            )?;
-            Ok(())
-        }
-        */
-
-
-    }
+}
